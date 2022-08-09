fa4733d5975a537eb414c5e567262c0595744d24
Max Hughson <MaxH@anteccontrols.com>
2022-08-09 10:29:42 -0500

    Add my first 2 examples

---
+++ examples/string/ex-etl-string.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include "../../etl_cpp/include/etl/string.h"
+
+constexpr size_t MAX_STRING_LENGTH = 150;
+
+int main()
+{
+    std::string question = "But how, my lord, shall we resist it now?";
+    etl::string<MAX_STRING_LENGTH> answer = "It must be thought on. If it pass against us";
+    int stack_variable = 36;
+
+    auto ptr_to_stack_int = &stack_variable;
+    auto ptr_to_std_data = (int const *)question.data();
+    auto ptr_to_etl_data = (int const *)answer.data();
+    auto ptr_to_etl_end = (int const *)answer.data_end();
+    
+
+    std::cout << "std::string question:\n\t" << question << '\n';
+    std::cout << "etl::string answer:\n\t" << answer.c_str() << '\n';
+
+    std::cout << "\nRight after initialization...\n";
+    std::cout << "std::string question:\n\t" << question << '\n';
+    std::cout << "etl::string answer:\n\t" << answer.c_str() << '\n';
+
+    std::cout << '\n';
+    std::cout << "std::string lives at " << ptr_to_std_data << " <- That's on the heap!\n";
+    std::cout << "stack int   lives at " << ptr_to_stack_int << '\n';
+    std::cout << "etl::string lives at " << ptr_to_etl_data << " <- That's on the stack!\n";
+    std::cout << "etl::string stops at " << ptr_to_etl_end << '\n';
+
+    std::cout << "\nAdding words...\n";
+
+    question += "How did this offer seem received, my lord?";
+    answer += ", We lose the better half of our possession";
+    ptr_to_std_data = (int const *)question.data();
+    ptr_to_etl_data = (int const *)answer.data();
+    ptr_to_etl_end = (int const *)answer.data_end();
+
+    std::cout << "std::string question:\n\t" << question << '\n';
+    std::cout << "etl::string answer:\n\t" << answer.c_str() << '\n';
+
+    std::cout << '\n';
+    std::cout << "std::string lives at " << ptr_to_std_data << " <- Extra text forced a new heap allocation.\n";
+    std::cout << "stack int   lives at " << ptr_to_stack_int << '\n';
+    std::cout << "etl::string lives at " << ptr_to_etl_data << " <- Extra text lives in the same stack buffer\n";
+    std::cout << "etl::string stops at " << ptr_to_etl_end << " <- Hey, that changed!\n";
+
+    std::cout << "\nOverwriting text...\n";
+
+    question = "What was the impediment that broke this off?";
+    answer = "The French ambassador upon that instant craved audience";
+    ptr_to_std_data = (int const *)question.data();
+    ptr_to_etl_data = (int const *)answer.data();
+    ptr_to_etl_end = (int const *)answer.data_end();
+    std::cout << "std::string question:\n\t" << question << '\n';
+    std::cout << "etl::string answer:\n\t" << answer.c_str() << '\n';
+
+    std::cout << '\n';
+    std::cout << "std::string lives at " << ptr_to_std_data << " <- Overwriting used the same heap allocation.\n";
+    std::cout << "stack int   lives at " << ptr_to_stack_int << '\n';
+    std::cout << "etl::string lives at " << ptr_to_etl_data << " <- New text lives in the same stack buffer\n";
+    std::cout << "etl::string stops at " << ptr_to_etl_end << "\n";
+
+    return (0);
+}
---
+++ examples/vector/ex-etl-vector.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include "../../etl_cpp/include/etl/vector.h"
+
+struct Monkey
+{
+    double height = 5.2;
+    int n_bananas = 1;
+    void Grow() { height = 1.5 * (height + 1.0); }
+    void Feed(int more_bananas) { n_bananas += more_bananas; }
+    void Eat() { n_bananas = std::max(n_bananas - 1, 0); }
+    Monkey(double c_height, int c_n_bananas) : height(c_height), n_bananas(c_n_bananas) {}
+};
+
+std::ostream &operator<<(std::ostream &os, Monkey const &monkey)
+{
+    os << "Height:\t" << monkey.height << "\tBananas:\t" << monkey.n_bananas;
+    return (os);
+}
+
+constexpr size_t MAX_NUM_MONKEYS = 3;
+
+int main()
+{
+    // std::vector<Monkey> barrel;
+    etl::vector<Monkey, MAX_NUM_MONKEYS> barrel;
+    barrel.push_back(Monkey(111.1, 1));
+    std::cout << "Look at all my monkeys!\n";
+    for (Monkey const &monkey : barrel)
+    {
+        std::cout << monkey << '\n';
+    }
+
+    std::cout << "\nA new monkey joins the party\n";
+    barrel.push_back(Monkey(222.2, 2));
+
+    std::cout << "\nLook at all my monkeys!\n";
+    for (Monkey const &monkey : barrel)
+    {
+        std::cout << monkey << '\n';
+    }
+    return (0);
+}
